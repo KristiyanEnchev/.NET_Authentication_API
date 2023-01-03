93e821d05dbee1a0c4aed55510d3ed734e77b89d
Kiko <kristiqnenchevv@gmail.com>
2023-01-03 14:41:28 +0200

    Change Host and Web responsibilities

--- Authentication.API/API/Host/Program.cs
+++ Authentication.API/API/Host/Program.cs
@@ -1,25 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Host
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    using Microsoft.AspNetCore.Builder;
+
+    using Web;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddWeb(builder.Configuration);
+
+            var app = builder.Build();
+
+            app.UseWeb();
+            app.MapEndpoints();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Authentication.API/API/Web/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace Web
+{
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddWeb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddHttpContextAccessor();
+            services.AddControllers().AddApplicationPart(Assembly.GetExecutingAssembly());
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseWeb(this IApplicationBuilder builder)
+        {
+            builder.UseSwagger()
+                    .UseSwaggerUI()
+                    .UseStaticFiles()
+                    .UseHttpsRedirection()
+                    .UseRouting()
+                    .UseAuthentication()
+                    .UseAuthorization();
+
+            return builder;
+        }
+
+        public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder)
+        {
+            builder.MapControllers()
+                    .RequireAuthorization();
+
+            return builder;
+        }
+    }
+}
