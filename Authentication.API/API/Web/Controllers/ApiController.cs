27aac21149d88fde973ad3fb3ee35e853b616e45
Kiko <kristiqnenchevv@gmail.com>
2023-01-04 15:51:20 +0200

    Add Base Controler and folders

---
+++ Authentication.API/API/Web/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        protected const string Id = "{id}";
+        protected const string PathSeparator = "/";
+    }
+}
