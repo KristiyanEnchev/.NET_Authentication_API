398136f937e3a6ca193c0788f00dd13b59af5f62
Kiko <kristiqnenchevv@gmail.com>
2023-01-05 16:36:25 +0200

    Configure dependency injection for WEB Application and Persistence

---
+++ Authentication.API/Core/Application/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace Application
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Authentication.API/Infrastructure/Persistence/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace Persistence
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
