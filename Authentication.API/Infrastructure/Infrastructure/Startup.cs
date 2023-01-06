3429157d4bc28ff4883fe6fc10091dc88e611f39
Kiko <kristiqnenchevv@gmail.com>
2023-01-06 12:41:56 +0200

    Set Up infrastructure

---
+++ Authentication.API/Infrastructure/Infrastructure/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Startup
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddServices(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
