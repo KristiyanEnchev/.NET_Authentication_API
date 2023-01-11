becf5d08050fe39030afac4bc01fca2c63885c26
Kiko <kristiqnenchevv@gmail.com>
2023-01-11 12:49:45 +0200

    Implement Graceful shutdown pattern with Serilog

--- Authentication.API/API/Host/Program.cs
+++ Authentication.API/API/Host/Program.cs
@@ -2,22 +2,42 @@ namespace Host
 {
     using Microsoft.AspNetCore.Builder;
 
+    using Serilog;
+
     using Web;
+    using Web.Extentions.Logging;
 
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            StaticLogger.EnsureInitialized();
+            Log.Information("Server Booting Up...");
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddWeb(builder.Configuration);
+                builder.RegisterSerilog();
+                builder.Services.AddWeb(builder.Configuration);
 
-            var app = builder.Build();
+                var app = builder.Build();
 
-            app.UseWeb();
-            app.MapEndpoints();
+                app.UseWeb();
+                app.MapEndpoints();
 
-            app.Run();
+                app.Run();
+            }
+            catch (Exception ex) when (!ex.GetType().Name.Equals("HostAbortedException", StringComparison.Ordinal))
+            {
+                StaticLogger.EnsureInitialized();
+                Log.Fatal(ex, "Unhandled exception");
+            }
+            finally
+            {
+                StaticLogger.EnsureInitialized();
+                Log.Information("Server Shutting down...");
+                Log.CloseAndFlush();
+            }
         }
     }
 }
