3ac0353b1a5099aee5e19d299cd15de472fd245a
Kiko <kristiqnenchevv@gmail.com>
2023-01-17 13:52:18 +0200

    Add Fluent Validation Configuration

--- Authentication.API/Core/Application/Startup.cs
+++ Authentication.API/Core/Application/Startup.cs
@@ -4,19 +4,25 @@
 
     using Microsoft.Extensions.DependencyInjection;
 
+    using FluentValidation;
 
     public static class Startup
     {
         public static void AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper();
-
+            services.AddValidators();
         }
 
         private static void AddAutoMapper(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
         }
+        private static void AddValidators(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
 
     }
 }
