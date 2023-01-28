d15e75b5098894ccda6e0aff4b4cf3eb00d40bf2
Kiko <kristiqnenchevv@gmail.com>
2023-01-28 14:18:37 +0200

    Add Domain Dispatcher

---
+++ Authentication.API/Core/Domain/Common/Interfaces/IDomainEventDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Common.Interfaces
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAndClearEvents(IEnumerable<BaseEntity> entitiesWithEvents);
+    }
+}
