14123ff1352c410fafb752d6f50a54faadfa1cec
Kiko <kristiqnenchevv@gmail.com>
2023-01-30 16:41:15 +0200

    Add DB Context

---
+++ Authentication.API/Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿namespace Persistence.Contexts
+{
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+    using Domain.Entities.Identity;
+
+    public class ApplicationDbContext :
+        IdentityDbContext<
+            User,
+            UserRole,
+            string,
+            IdentityUserClaim<string>,
+            IdentityUserRole<string>,
+            IdentityUserLogin<string>,
+            RoleClaim,
+            IdentityUserToken<string>>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserRole> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
