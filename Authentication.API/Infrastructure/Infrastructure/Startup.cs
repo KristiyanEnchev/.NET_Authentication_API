ff43fd9b767a516bcdc772a9dddfde28aa8d1e20
Kiko <kristiqnenchevv@gmail.com>
2023-02-04 16:47:32 +0200

    Apply the current Services

--- Authentication.API/Infrastructure/Infrastructure/Startup.cs
+++ Authentication.API/Infrastructure/Infrastructure/Startup.cs
@@ -3,6 +3,15 @@
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Domain.Common;
+    using Domain.Common.Interfaces;
+
+    using Infrastructure.Services;
+
     public static class Startup
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
@@ -12,9 +21,12 @@
             return services;
         }
 
-        private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        private static void AddServices(this IServiceCollection services, IConfiguration configuration)
         {
-            return services;
+            services
+                .AddTransient<IMediator, Mediator>()
+                .AddTransient<IDomainEventDispatcher, DomainEventDispatcher>()
+                .AddTransient<IDateTimeService, DateTimeService>();
         }
     }
 }
