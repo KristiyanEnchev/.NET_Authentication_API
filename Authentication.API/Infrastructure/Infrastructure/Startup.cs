02fb9d2b7979db013ba8d673ce385a8e48201938
Kiko <kristiqnenchevv@gmail.com>
2023-02-05 12:36:21 +0200

    Add Identity Configuration

--- Authentication.API/Infrastructure/Infrastructure/Startup.cs
+++ Authentication.API/Infrastructure/Infrastructure/Startup.cs
@@ -1,5 +1,6 @@
 ﻿namespace Infrastructure
 {
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -8,15 +9,19 @@
     using Application.Interfaces;
 
     using Domain.Common;
+    using Domain.Entities.Identity;
     using Domain.Common.Interfaces;
 
     using Infrastructure.Services;
+
+    using Persistence.Contexts;
 
     public static class Startup
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddServices(configuration);
+            services.AddIdentity(configuration);
 
             return services;
         }
@@ -27,6 +32,21 @@
                 .AddTransient<IMediator, Mediator>()
                 .AddTransient<IDomainEventDispatcher, DomainEventDispatcher>()
                 .AddTransient<IDateTimeService, DateTimeService>();
+        }
+
+        private static void AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddIdentity<User, UserRole>(options =>
+                {
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddTokenProvider("Authentication.Api", typeof(DataProtectorTokenProvider<User>));
         }
     }
 }
