144e6627552b05b7d3cba95641708fdc28538f00
Kiko <kristiqnenchevv@gmail.com>
2023-02-13 15:55:17 +0200

    Add Identity Controller

---
+++ Authentication.API/API/Web/Controllers/Identity/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿namespace Web.Controllers.Identity
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using Application.Identity.Commands.Register;
+
+    using Swashbuckle.AspNetCore.Annotations;
+
+    using Web.Extentions;
+
+    public class IdentityController : ApiController
+    {
+        [HttpPost(nameof(Register))]
+        [AllowAnonymous]
+        [SwaggerOperation("Register a user.", "")]
+        public async Task<ActionResult<string>> Register(UserRegisterCommand request)
+        {
+            return await Mediator.Send(request).ToActionResult();
+        }
+    }
+}
---
+++ Authentication.API/API/Web/Extentions/ResultExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Web.Extentions
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Shared;
+
+    public static class ResultExtensions
+    {
+        public static async Task<ActionResult<TData>> ToActionResult<TData>(this Task<Result<TData>> resultTask)
+        {
+            var result = await resultTask;
+
+            if (!result.Succeeded)
+            {
+                if (result.Exception != null)
+                {
+                    return new BadRequestObjectResult(new { error = result.Exception.Message });
+                }
+                return new BadRequestResult();
+            }
+
+            if (result.Data == null && result.Messages.Any())
+            {
+                return new OkObjectResult(new { message = result.Messages.FirstOrDefault() });
+            }
+            else if (result.Data == null)
+            {
+                return new NoContentResult();
+            }
+
+            return new OkObjectResult(new { data = result.Data, message = result.Messages.FirstOrDefault() });
+        }
+
+    }
+}
