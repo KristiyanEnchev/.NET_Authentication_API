15a90c76b70c3dcb3692b9f2670e63f966e2eb74
Kiko <kristiqnenchevv@gmail.com>
2023-02-20 12:43:42 +0200

    Add Refresh Comand

---
+++ Authentication.API/Core/Application/Identity/Commands/Refresh/UserRefreshCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace Application.Identity.Commands.Refresh
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using MediatR;
+
+    using Application.Interfaces;
+    using Application.Identity.Commands.Common;
+
+    public class UserRefreshCommand : UserRefreshModel, IRequest<UserResponseModel>
+    {
+        public UserRefreshCommand(string refreshToken)
+            : base(refreshToken)
+        {
+        }
+
+        public class UserRefreshCommandHandler : IRequestHandler<UserRefreshCommand, UserResponseModel>
+        {
+            private readonly IIdentity identity;
+
+            public UserRefreshCommandHandler(IIdentity identity)
+            {
+                this.identity = identity;
+            }
+
+            public async Task<UserResponseModel> Handle(UserRefreshCommand request, CancellationToken cancellationToken)
+            {
+                var result = await identity.RefreshTokenAsync(request.RefreshToken);
+
+                return result.Data;
+            }
+        }
+    }
+}
