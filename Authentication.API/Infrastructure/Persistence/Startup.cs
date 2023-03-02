58b15fc93f9761ce2549fc63d9903917d1511fe8
Kiko <kristiqnenchevv@gmail.com>
2023-03-02 12:50:54 +0200

    Apply Repositories to the Dependency scope

--- Authentication.API/Infrastructure/Persistence/Startup.cs
+++ Authentication.API/Infrastructure/Persistence/Startup.cs
@@ -5,12 +5,16 @@
     using Microsoft.Extensions.DependencyInjection;
 
     using Persistence.Contexts;
+    using Persistence.Repositories;
+
+    using Application.Interfaces.Repositories;
 
     public static class Startup
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext(configuration);
+            services.AddRepositories();
 
             return services;
         }
@@ -24,6 +28,12 @@
                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<ApplicationDbContextInitialiser>();
+        }
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services
+                .AddTransient(typeof(IBaseRepository), typeof(BaseRepository))
+                .AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         }
     }
 }
