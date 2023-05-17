9005d8dee8d1cbba0226d35ea9c8d30202fc5fd0
Kiko <kristiqnenchevv@gmail.com>
2023-05-17 15:33:53 +0300

    Implement Confirm email comand

--- Authentication.API/Core/Application/Handlers/Identity/Commands/User/ConfirmEmailCommand.cs
+++ Authentication.API/Core/Application/Handlers/Identity/Commands/User/ConfirmEmailCommand.cs
@@ -1,6 +1,33 @@
 ﻿namespace Application.Handlers.Identity.Commands.User
 {
-    public class ConfirmEmailCommand
+    using MediatR;
+
+    using Shared;
+
+    using Application.Interfaces;
+
+    public class ConfirmEmailCommand : IRequest<Result<string>>
     {
+        public string? Email { get; set; }
+        public string? Code { get; set; }
+        public string? Otp { get; set; }
+
+
+        public class ConfirmEmailCommandHandler : IRequestHandler<ConfirmEmailCommand, Result<string>> 
+        {
+            private readonly IIdentity _identity;
+
+            public ConfirmEmailCommandHandler(IIdentity identity)
+            {
+                _identity = identity;
+            }
+
+            public async Task<Result<string>> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken) 
+            {
+                var result = await _identity.ConfirmEmail(request.Email!, request.Code!, request.Otp!);
+
+                return result;
+            }
+        }
     }
 }
