315c6cb8c2d05d9eb1cc128150cdd537d47b3dd3
Kiko <kristiqnenchevv@gmail.com>
2023-05-18 16:20:30 +0300

    Implement Disable Two Factor Authentication command

--- Authentication.API/Core/Application/Handlers/Identity/Commands/User/DisableTwoFactorAuthenticationCommand.cs
+++ Authentication.API/Core/Application/Handlers/Identity/Commands/User/DisableTwoFactorAuthenticationCommand.cs
@@ -1,6 +1,30 @@
 ﻿namespace Application.Handlers.Identity.Commands.User
 {
-    public class DisableTwoFactorAuthenticationCommand
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Shared;
+
+    public class DisableTwoFactorAuthenticationCommand : IRequest<Result<string>>
     {
+        public string? Email { get; set; }
+
+        public class DisableTwoFactorAuthenticationCommandHandler : IRequestHandler<DisableTwoFactorAuthenticationCommand, Result<string>>
+        {
+            private readonly IIdentity _identity;
+
+            public DisableTwoFactorAuthenticationCommandHandler(IIdentity identity)
+            {
+                _identity = identity;
+            }
+
+            public async Task<Result<string>> Handle(DisableTwoFactorAuthenticationCommand request, CancellationToken cancellationToken) 
+            {
+                var result = await _identity.DisableTwoFactorAuthentication(request.Email!);
+
+                return result;
+            }
+        }
     }
 }
