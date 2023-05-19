72beb7cc17f30f7bab3579d8a03b84e7570063a6
Kiko <kristiqnenchevv@gmail.com>
2023-05-19 12:39:46 +0300

    Add Enable Two Factor Authentication Command

--- Authentication.API/Core/Application/Handlers/Identity/Commands/User/EnableTwoFactorAuthenticationCommand.cs
+++ Authentication.API/Core/Application/Handlers/Identity/Commands/User/EnableTwoFactorAuthenticationCommand.cs
@@ -1,6 +1,31 @@
 ﻿namespace Application.Handlers.Identity.Commands.User
 {
-    public class EnableTwoFactorAuthenticationCommand
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Shared;
+
+    public class EnableTwoFactorAuthenticationCommand : IRequest<Result<string>>
+    {
+        public string? Email { get; set; }
+
+        public class EnableTwoFactorAuthenticationCommandHandler : IRequestHandler<EnableTwoFactorAuthenticationCommand, Result<string>>
+        {
+            private readonly IIdentity _identity;
+
+            public EnableTwoFactorAuthenticationCommandHandler(IIdentity identity)
+            {
+                _identity = identity;
+            }
+
+            public async Task<Result<string>> Handle(EnableTwoFactorAuthenticationCommand request, CancellationToken cancellationToken)
+            {
+                var result = await _identity.EnableTwoFactorAuthentication(request.Email!);
+
+                return result;
+            }
+        }
     {
     }
 }
