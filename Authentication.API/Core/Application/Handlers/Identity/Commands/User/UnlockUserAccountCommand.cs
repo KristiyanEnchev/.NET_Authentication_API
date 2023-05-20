f9673967c23398c00186897bc95d1cb786c116f8
Kiko <kristiqnenchevv@gmail.com>
2023-05-20 13:54:15 +0300

    Add Unlock User Account Command

--- Authentication.API/Core/Application/Handlers/Identity/Commands/User/UnlockUserAccountCommand.cs
+++ Authentication.API/Core/Application/Handlers/Identity/Commands/User/UnlockUserAccountCommand.cs
@@ -1,6 +1,30 @@
 ﻿namespace Application.Handlers.Identity.Commands.User
 {
-    public class UnlockUserAccountCommand
+    using MediatR;
+
+    using Application.Interfaces;
+
+    using Shared;
+
+    public class UnlockUserAccountCommand : IRequest<Result<string>>
     {
+        public string? Email { get; set; }
+
+        public class UnlockUserAccountCommandHandler : IRequestHandler<UnlockUserAccountCommand, Result<string>>
+        {
+            private readonly IIdentity _identity;
+
+            public UnlockUserAccountCommandHandler(IIdentity identity)
+            {
+                _identity = identity;
+            }
+
+            public async Task<Result<string>> Handle(UnlockUserAccountCommand request, CancellationToken cancellationToken) 
+            {
+                var result = await _identity.UnlockUserAccount(request.Email!);
+
+                return result;
+            }
+        }
     }
 }
